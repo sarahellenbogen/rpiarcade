c0caf7fa67b441fc62d1f5db18e1312af00320e7
Sarah Ellenbogen <sarah@garb-o-liner.com>
2022-12-03 22:27:28 +0000

    updated vend_code

--- vend_code.py
+++ vend_code.py
@@ -1,3 +1,4 @@
+
 import pygame     # Import pygame graphics library
 from pygame.locals import *
 import RPi.GPIO as GPIO
@@ -14,7 +15,7 @@ DIRA2 = 13
 DIRB1 = 21
 DIRB2 = 20
 
-# GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BCM)
 
 # Setup motor pins
 # GPIO.setup(DIRA1, GPIO.OUT) # DIRA1
@@ -50,8 +51,8 @@ os.putenv('SDL_MOUSEDEV', '/dev/input/touchscreen')
 def GPIO17_callback(channel):
     GPIO.cleanup()
     sys.exit()
-# GPIO.setup(17, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-# GPIO.add_event_detect(17, GPIO.FALLING, callback=GPIO17_callback, bouncetime=100) 
+GPIO.setup(17, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.add_event_detect(17, GPIO.FALLING, callback=GPIO17_callback, bouncetime=100) 
 
 
 pygame.init()
@@ -60,14 +61,16 @@ size = 240,320
 black = 0, 0, 0
 gray = 100, 100, 100
 white = 255, 255, 255
+red = 255, 0, 0
 screen = pygame.display.set_mode(size)
 my_font = pygame.font.Font(None, 40)
+message_font = pygame.font.Font(None, 30)
 
 # Read for reference: https://pygame-zero.readthedocs.io/en/stable/ptext.html
 # create buttons
-my_buttons = {  'A':( 30,  220),   'B':( 30, 260),   'C':( 30, 300),
-                '1':( 70,  220),   '2':( 70, 260),   '3':( 70, 300),
-              'CLR':(130,  220), 'SEL':(130, 260),'QUIT':(130, 300)}
+my_buttons = {  'A':( 50,  220 ),   'B':( 50, 300 ),
+                '1':( 120,  220 ),   '2':( 120, 300 ),
+              'CLR':( 190,  220 ), 'SEL':( 190, 300 )}
 rects = []
 surface = []
 for my_text, text_pos in my_buttons.items():
@@ -82,8 +85,13 @@ for my_text, text_pos in my_buttons.items():
 state = 0
 code = ["_", "_"]
 i=0
+tickets = 0
+message = "Scan card or pick item"
+ticketStr = "No card scanned"
+popUp = " "
+canSelect = 1
 clock = pygame.time.Clock()
-end_time = time.time() + 30
+end_time = time.time() + 120
 while (time.time() < end_time):
 
     screen.fill(white)               # Erase the Work space
@@ -94,69 +102,111 @@ while (time.time() < end_time):
     num_rect = num_surface.get_rect(center=(60, 180))
     screen.blit(num_surface, num_rect)
 
-    for event in pygame.event.get():
-        if(event.type is MOUSEBUTTONUP):
-            pos = pygame.mouse.get_pos()
-            print(pos)
-            pos = pos[::-1]
-            if   rects[0].collidepoint(pos):
-                print("A")
-                print(code[0] + code[1])
-                if (code[0] == "_") :
-                    code[0] = "A"
-                elif (code[1] == "_") :
-                    code[1] = "A"
+    num_surface = message_font.render((ticketStr), True, black)
+    num_rect = num_surface.get_rect(center=(120, 40))
+    screen.blit(num_surface, num_rect)
 
-            elif rects[1].collidepoint(pos):
-                print("B")
-                print(code[0] + code[1])
-                if (code[0] == "_") :
-                    code[0] = 'B'
-                elif (code[1] == "_") :
-                    code[1] = 'B'
+    num_surface = message_font.render((message), True, black)
+    num_rect = num_surface.get_rect(center=(120, 100))
+    screen.blit(num_surface, num_rect)
 
-            elif rects[2].collidepoint(pos):
-                print("C")
-                print(code[0] + code[1])
-                if (code[0] == "_") :
-                    code[0] = "C"
-                elif (code[1] == "_") :
-                    code[1] = "C"
+    num_surface = my_font.render((popUp), True, red)
+    num_rect = num_surface.get_rect(center=(120, 150))
+    screen.blit(num_surface, num_rect)
 
-            elif rects[3].collidepoint(pos):
-                print("1")
-                print(code[0] + code[1])
-                if (code[0] == "_") :
-                    code[0] = "1"
-                elif (code[1] == "_") :
-                    code[1] = "1"
+    if state == 0:
+        message = "Scan card or pick item"
+        ticketStr = "No card scanned"
+        popUp = " "
+        canSelect = 1
+        #RFID read here if read tickets == read
 
-            elif rects[4].collidepoint(pos):
-                print("2")
-                print(code[0] + code[1])
-                if (code[0] == "_") :
-                    code[0] = "2"
-                elif (code[1] == "_") :
-                    code[1] = "2"
+    elif state == 1:
+        popUp = " "
 
-            elif rects[5].collidepoint(pos):
-                print("3")
-                print(code[0] + code[1])
-                if (code[0] == "_") :
-                    code[0] = "3"
-                elif (code[1] == "_") :
-                    code[1] = "3"
 
-            elif rects[6].collidepoint(pos):
-                code = ["_", "_"]
-                print("CLR")
-            elif rects[7].collidepoint(pos):
-                code = ["_", "_"]
-                print("SEL")
-            elif rects[8].collidepoint(pos):
-                print("QUIT")
-                GPIO.cleanup()
-                sys.exit()
+    elif state == 2:
+        popUp = "SCAN CARD"
+        canSelect = 0
+        if (time.time() >= state2start + 5):
+            state = 0 
+
+    elif state == 3:
+        popUp = " "
+        canSelect = 0
+
+    elif state == 4:
+        popUp = "VENDING"
+        canSelect = 0
+
+    if canSelect == 1:
+
+        for event in pygame.event.get():
+            if(event.type is MOUSEBUTTONUP):
+                x,y = pygame.mouse.get_pos()
+                pos = pygame.mouse.get_pos()
+                print(pos)
+
+                if x >= 175 and x <= 205 and y >= 80 and y <= 115:
+                    print("A")
+                    print(code[0] + code[1])
+                    if (code[0] == "_") :
+                        code[0] = "A"
+                    elif (code[1] == "_") :
+                        code[1] = "A"
+
+                elif x >= 175 and x <= 205 and y >= 0 and y <= 30:
+                    print("B")
+                    print(code[0] + code[1])
+                    if (code[0] == "_") :
+                        code[0] = 'B'
+                    elif (code[1] == "_") :
+                        code[1] = 'B'
+
+                elif x >= 105 and x <= 135 and y >= 80 and y <= 115:
+                    print("1")
+                    print(code[0] + code[1])
+                    if (code[0] == "_") :
+                        code[0] = "1"
+                    elif (code[1] == "_") :
+                        code[1] = "1"
+
+                elif x >= 105 and x <= 135 and y >= 0 and y <= 30:
+                    print("2")
+                    print(code[0] + code[1])
+                    if (code[0] == "_") :
+                        code[0] = "2"
+                    elif (code[1] == "_") :
+                        code[1] = "2"
+
+                elif x >= 10 and x <= 75 and y >= 80 and y <= 115:
+                    code = ["_", "_"]
+                    print("CLR")
+
+                elif x >= 10 and x <= 75 and y >= 0 and y <= 30:
+                    if code[0] == "A":
+                        if code[1] == "1":
+                            message = "This prize is 10 tickets"
+                            state = 2
+                        if code[1] == "2":
+                            message = "This prize is 10 tickets"
+                            state = 2
+
+                    if code[0] == "B":
+                        if code[1] == "1":
+                            message = "This prize is 10 tickets"
+                            state = 2
+                        if code[1] == "2":
+                            message = "This prize is 10 tickets"
+                            state == 2
+
+                    if state != 2:
+                        state = 0
+                    else:
+                        state2start = time.time()
+
+                    code = ["_", "_"]
+                    print("SEL")
     
     # Render
     screen.blit(pygame.transform.rotate(screen, 180), (0, 0))
